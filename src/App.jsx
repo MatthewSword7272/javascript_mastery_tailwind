d73e5c0e9bbb4eed36e867ea50ac2fdb66bc17d4
Matthew Catalfamo <sword1005@hotmail.com.au>
2025-01-17 16:27:32 +1100

    Added Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
 import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header></Header>
+        <HeroSection />
       </div>
       <ButtonGradient />
     </>
